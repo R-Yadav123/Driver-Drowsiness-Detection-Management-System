6826003036e347846ee0e5b85e8055c8c18445e1
Rhea Yadav <rheayadav@MacBook-Pro.local>
2025-12-02 17:35:46 -0500

    Added SQL queries and updated files

--- DriverDatabase.py
+++ DriverDatabase.py
@@ -2,7 +2,6 @@
 # store records and information
 # Connects with sqlite in order to execute SQL statements
 
-
 import sqlite3
 import os
 import pandas as pd
@@ -114,11 +113,110 @@ df_event.to_sql("Driver_Drowsiness_Event", conn, if_exists="append", index=False
 conn.commit()
 
 print("All files there success!")
+print(" ")
+# SQL queries 
+print("Get all of the trips with Driver's Name and Vehicle Model:\n")
+
+query = """
+SELECT 
+    Trip.Trip_ID,
+    Driver.Name AS Driver_Name,
+    Vehicle.Model AS Vehicle_Model,
+    Trip.Location_Start,
+    Trip.Location_End,
+    Trip.Miles_Traveled
+FROM Trip
+JOIN Driver ON Trip.Driver_ID = Driver.ID
+LEFT JOIN Vehicle ON Trip.Vehicle_ID = Vehicle.Vehicle_ID;
+
+"""
+
+df_result = pd.read_sql_query(query, conn)
+
+print(df_result)
+print(" ")
+
+print("Get all vehicle information whose make is Tesla and model type is Truck:\n")
+query = """
+SELECT 
+    v.Vehicle_ID, v.Driver_ID, v.Model, v.Make, v.Year, v.License_Plate_Num, v.Vehicle_Type
+FROM Vehicle v WHERE v.Make = 'Tesla' AND v.Vehicle_Type = 'Truck';
+
+"""
+
+df_result = pd.read_sql_query(query, conn)
+
+print(df_result)
+
+
+print(" ")
+
+print("Left Join: Trips with drivers, even if there is vehicle information that is missing or null: \n")
+query = """
+SELECT tr.Trip_ID, ve.Model, dr.Name, tr.Miles_Traveled FROM Trip tr 
+INNER JOIN Driver dr ON tr.Driver_ID = dr.ID LEFT JOIN Vehicle ve ON tr.Vehicle_ID = ve.Vehicle_ID;
+"""
+
+df_result = pd.read_sql_query(query, conn)
+
+print(df_result)
+
+print(" ")
+
+print("Trips longer than 350 miles: \n")
+query = """
+SELECT Driver_ID, Miles_Traveled, Trip_ID from Trip WHERE Miles_Traveled > 350
+ORDER BY Miles_Traveled ASC;
+"""
+
+df_result = pd.read_sql_query(query, conn)
+
+print(df_result)
+
+print(" ")
+
+print("Count the number of duplicate driver's names in ascending order: \n")
+query = """
+SELECT Name, COUNT(*) AS name_counter FROM Driver GROUP BY Name
+HAVING COUNT(*) > 1 ORDER BY name_counter ASC;
+"""
+
+df_result = pd.read_sql_query(query, conn)
+
+print(df_result)
+
+
+print(" ")
+print("Count of trips per vehicle: \n")
+query = """
+SELECT v.Model, COUNT(t.Trip_ID) AS Trip_Count
+FROM Trip t
+INNER JOIN Vehicle v ON t.Vehicle_ID = v.Vehicle_ID GROUP BY v.Model
+ORDER BY Trip_Count ASC;
+
+"""
+df_result = pd.read_sql_query(query, conn)
+
+print(df_result)
 
 
 
 
+print(" ")
+print("Average drowsiness level per driver, no duplicates: \n")
+query = """
+SELECT d.Name, AVG(e.Level_Of_Drowsiness) AS Avg_Drowsiness
+FROM Driver_Drowsiness_Event e
+INNER JOIN Driver d ON e.Driver_ID = d.ID
+GROUP BY d.Name
+ORDER BY Avg_Drowsiness ASC;
 
-# already inserted records
-# now clear all the tables and use a dataset for soe of the values 
-# SQL Joins, filtering, cleaning, etc. , use a dataset
+"""
+df_result = pd.read_sql_query(query, conn)
+
+print(df_result)
+
+print(" ")
+
+# TO DO: 
+#plots and create a prediction model (training/testing data)
